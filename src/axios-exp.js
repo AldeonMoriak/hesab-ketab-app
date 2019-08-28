772c1eb2f0b7aacabefe1abe6db0d24b8c27b654
Aldeon Moriak <aldeonmoriak@gmail.com>
2019-08-28 23:13:23 +0430

    add axios and tried to connect to the server and failed

--- src/axios-exp.js
+++ src/axios-exp.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://aldeon-react-burger.firebaseio.com/"
+  baseURL: "http://192.168.1.151:75"
 });
+// instance.defaults.headers.post["Content-Type"] = "application/json";
+instance.defaults.headers.post["Access-Control-Allow-Origin"] = "*";
+// instance.defaults.headers.post["Vary"] = "Origin";
 
 export default instance;
--- src/containers/Expense/Expense.js
+++ src/containers/Expense/Expense.js
@@ -1,27 +1,70 @@
 import React, { Component } from "react";
 import classes from "./Expense.module.css";
+import Button from "@material-ui/core/Button";
+import axios from "../../axios-exp";
 
 class Expense extends Component {
+  state = {
+    elements: {
+      ID: 0,
+      SpenderId: 1,
+      Price: 0,
+      StuffName: "",
+      Description: "",
+      type: 0
+    }
+  };
+
+  clickHandler = () => {
+    console.log(this.state.elements);
+    let expense = this.state.elements;
+    axios.post("/api/HouseCost/InsertSell", expense).then(response => {
+      console.log("response: ", response).catch(error => {
+        console.log("error:", error);
+      });
+    });
+  };
+
+  changeHandler = event => {
+    const element = event.target.name;
+    const updatedState = { ...this.state.elements };
+    // if (element === "type") {
+    //   updatedState[element].push(event.target.value);
+    // } else {
+    updatedState[element] = event.target.value;
+    // }
+    this.setState({ elements: updatedState });
+  };
+
   render() {
     return (
       <div className={classes.Expense}>
-        <form name="kharj" action="get">
-          مادرخرج:{" "}
-          <select form="kharj" name="madar-kharj">
-            <option value="ashkan">اشکان</option>
-            <option value="ashkan">امین</option>
-            <option value="ashkan">مهران</option>
+        <form name="expense" action="post">
+          Spender:{" "}
+          <select form="expense" name="SpenderId" onChange={this.changeHandler}>
+            <option value="1">امین</option>
+            <option value="2">اشکان</option>
+            <option value="3">مهران</option>
           </select>
-          جنس: <input type="text" />
-          قیمت: <input type="number" />
-          شرح: <input type="text" />
-          خورنده ها:
-          <select form="kharj" name="khorandeha" multiple size="3">
-            <option value="ashkan">اشکان</option>
-            <option value="ashkan">امین</option>
-            <option value="ashkan">مهران</option>
+          Stuff:{" "}
+          <input name="StuffName" type="text" onChange={this.changeHandler} />
+          Price:{" "}
+          <input name="Price" type="number" onChange={this.changeHandler} />
+          Description:{" "}
+          <input name="Description" type="text" onChange={this.changeHandler} />
+          Participants:{" "}
+          <select
+            form="expense"
+            name="type"
+            multiple
+            size="3"
+            onChange={this.changeHandler}
+          >
+            <option value="1">امین</option>
+            <option value="2">اشکان</option>
+            <option value="3">مهران</option>
           </select>
-          <input type="submit" value="ثبت" />
+          <Button variant="contained" onClick={this.clickHandler} />
         </form>
       </div>
     );
